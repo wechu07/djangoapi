22906474b6cac3c4a2f23b33e164bbcbcd04d2e0
wechu07 <wechu017@gmail.com>
2022-11-24 10:32:32 +0300

    adding views

--- blog/posts/api/views.py
+++ blog/posts/api/views.py
@@ -3,6 +3,13 @@ from . import serializers
 from rest_framework import generics, status
 from rest_framework.response import Response
 
-class PostListView(generics.ListAPIView):
+class PostCreateView(generics.CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = serializers.PostSerializer
+
+    def create(self, request, *args, **kwargs):
+        super(PostCreateView, self).create(request, args, kwargs)
+        response = {"status_code": status.HTTP_200_OK,
+                    "message": "Successfully created",
+                    "result": request.data}
+        return Response(response)
